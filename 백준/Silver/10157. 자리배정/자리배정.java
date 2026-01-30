dcf3986790b3fc2899e6d817882033d695b43a3d
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-01-31 03:39:52 +0900

    [Silver III] Title: 자리배정, Time: 80 ms, Memory: 12724 KB -BaekjoonHub

---
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int c = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        
+        int k = Integer.parseInt(br.readLine());
+
+        if (k > r * c) {
+        	System.out.print(0);
+        	return;
+        }
+        else if (k == 1) {
+            System.out.println("1 1");
+            return;
+        }
+        
+        int top = 0, bottom = r - 1;
+        int left = 0, right = c - 1;
+        
+        int cnt = 1;
+        while (top <= bottom && left <= right) {
+            // 위쪽 ↑
+            for (int i = bottom; i >= top; i--) {
+                if (cnt == k) {
+                    int x = left + 1;
+                    int y = r - i;
+                    System.out.println(x + " " + y);
+                    return;
+                }
+                cnt++;
+            }
+            left++;        
+            
+            // 오른쪽 →
+            for (int i = left; i <= right; i++) {
+                if (cnt == k) {
+                    int x = i + 1;
+                    int y = r - top;
+                    System.out.println(x + " " + y);
+                    return;
+                }
+                cnt++;
+            }
+            top++;
+
+            // 아래 ↓
+            for (int i = top; i <= bottom; i++) {
+                if (cnt == k) {
+                    int x = right + 1;
+                    int y = r - i;
+                    System.out.println(x + " " + y);
+                    return;
+                }
+                cnt++;
+            }
+            right--;
+            
+            // 왼쪽 ←
+            for (int i = right; i >= left; i--) {
+                if (cnt == k) {
+                    int x = i + 1;
+                    int y = r - bottom;
+                    System.out.println(x + " " + y);
+                    return;
+                }
+                cnt++;
+            }
+            bottom--;
+        }
+    }
+}
