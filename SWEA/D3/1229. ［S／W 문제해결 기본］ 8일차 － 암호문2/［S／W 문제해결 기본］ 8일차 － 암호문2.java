bfaa5ab0b8458bbdb1e0eb956acda84d3f8a58e7
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-02 21:01:58 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 83 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for (int t = 0; t < 10; t++) {
+        	int n = Integer.parseInt(br.readLine());
+            
+            // 암호문 - LinkedList 사용
+            List<String> cipher = new LinkedList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+            	cipher.add(st.nextToken());
+            }
+            
+            int cmdCnt = Integer.parseInt(br.readLine());
+            
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < cmdCnt; i++) {
+            	String cmd = st.nextToken(); // 명령 입력 (I | D)
+            	int param1 = Integer.parseInt(st.nextToken());
+            	int param2 = Integer.parseInt(st.nextToken());
+            	
+                if (cmd.equals("I")) {
+                	List<String> temp = new ArrayList<>(param2);
+                	for (int j = 0; j < param2; j++) {
+                		temp.add(st.nextToken());
+                	}
+                	cipher.addAll(param1, temp);
+                	
+                }
+                else { // cmd.equals("D")
+                	for (int j = 0; j < param2; j++) {
+                		cipher.remove(param1);
+                	}
+                } 
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append('#').append(t+1).append(' ');
+            for (int i = 0; i < 10; i++) {
+            	sb.append(cipher.get(i)).append(' ');
+            }   
+            System.out.println(sb);
+        }       
+    }
+}
