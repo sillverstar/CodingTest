9de6f30ff1a32ce8ab439778ee40883ad71e0743
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-02 22:55:25 +0900

    [Silver II] Title: DNA 비밀번호, Time: 160 ms, Memory: 20584 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
 	// 만들 수 있는 비밀번호인지 확인
 	static boolean check(int[] now, int[] acgt) {
 		for (int i = 0; i < 4; i++) {
@@ -10,6 +11,15 @@ public class Main {
 			}
 		}
 		return true;
+	}
+	
+	// ACGT에 따른 배열 위치 반환
+	static int idx(char c) {
+		if (c == 'A') { return 0; }
+		else if (c == 'C') { return 1; }
+		else if (c == 'G') { return 2; }
+		else { return 3; }
+		
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -34,10 +44,7 @@ public class Main {
 		// 초기 윈도우
 		for (int i = 0; i < p; i++) {
 			char c = dna.charAt(i);
-			if (c == 'A') { now[0]++; }
-			else if (c == 'C') { now[1]++; }
-			else if (c == 'G') { now[2]++; }
-			else { now[3]++; }
+			now[idx(c)]++;
 
 		}
 		
@@ -50,17 +57,11 @@ public class Main {
 		for (int i = p; i < s; i++) {
 			// 한 개 빼고 now 업데이트
 			char out = dna.charAt(i - p);
-			if (out == 'A') { now[0]--; }
-			else if (out == 'C') { now[1]--; }
-			else if (out == 'G') { now[2]--; }
-			else { now[3]--; }
-			
+			now[idx(out)]--;
+
 			// 한 개 넣고 now 업데이트
 			char in = dna.charAt(i);
-			if (in == 'A') { now[0]++; }
-			else if (in == 'C') { now[1]++; }
-			else if (in == 'G') { now[2]++; }
-			else { now[3]++; }
+			now[idx(in)]++;
 			
 			// 비밀번호 되나 체크
 			if (check(now, acgt)) {
