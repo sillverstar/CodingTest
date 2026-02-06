9b207e66c562371233e9378199233593daa8b1bd
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-06 20:51:06 +0900

    [D3] Title: 햄버거 다이어트, Time: 119 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,30 +1,22 @@
 import java.util.*;
 import java.io.*;
 
-// 조합 문제
-// 입력된 칼로리 이하로 먹을 때 점수가 가장 높은 재료의 조합을 탐색(중복x 순서x)
 public class Solution {
 	static int n; // 재료 수
 	static int limit; // 제한 칼로리
 	static int maxScore; // 최대 점수
 	static int[] scores; // 점수
-	static int[] cal; // 제한 칼로리
+	static int[] cal; // 칼로리
 	
-	public static void combination(int count, int score, int allCal) {
-		if(allCal > limit) { // 칼로리가 주어진 칼로리를 넘으면 끝
+	public static void combination(int start, int score, int allCal) { // 시작 지점 결정
+		if(allCal > limit) { // 칼로리가 목표치를 넘으면 끝
 			return;
 		}
-		
-        if (count == n) { // 재료 수를 다 채우면 최대 점수 갱신
-            maxScore = Math.max(maxScore, score);
-            return;
-        }
-		
-        // 현재 재료 선택하는 경우
-        combination(count + 1, score + scores[count], allCal + cal[count]);
+        maxScore = Math.max(maxScore, score);
 
-        // 현재 재료 선택 안 하는 경우
-        combination(count + 1, score, allCal);
+        for (int i = start; i < n; i++) { // 시작점을 +1 하면서 조합 요소 결정
+        	combination(i + 1, score + scores[i], allCal + cal[i]);
+        }
 	}
 
 	public static void main(String[] args) throws IOException {
