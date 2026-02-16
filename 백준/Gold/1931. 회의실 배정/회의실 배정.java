113a03c8f1f9baca72a5cf8b7f6266b1b867a138
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-17 00:29:33 +0900

    [Gold V] Title: 회의실 배정, Time: 504 ms, Memory: 44336 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n, maxCnt;
+	static Meeting[] meetings;
+	// 클래스 
+	static class Meeting implements Comparable<Meeting>{
+		// implements Comparable<T> : 내가 만든 Meeting 객체끼리 서로 어떻게 비교할지 내가 직접 정의해볼게!
+		// 기본 타입(int, double)은 값을 기준으로 비교하지만, 객체의 경우 어떻게 비교할지 정의해줘야 함
+		int start, end; // 시작 시간, 종료 시간
+
+		// 생성자
+		public Meeting(int start, int end) {
+			super();
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public String toString() {
+			return "Meeting [start=" + start + ", end=" + end + "]";
+		}
+		
+		@Override
+		public int compareTo(Meeting o) {
+			// 종료시간이 빠른 순
+			int diff = Integer.compare(this.end, o.end);
+			if (diff != 0) return diff;
+			 
+			// 종료 시간이 같다면 시작 시간이 빠른 순
+			return Integer.compare(this.start, o.start);
+		}
+	}
+	
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		n = Integer.parseInt(br.readLine());
+		maxCnt = 0;
+		
+		meetings = new Meeting[n];
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			meetings[i] = new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		}
+		
+		
+		getSchedule(meetings);
+		System.out.println(maxCnt);
+	}
+	
+	private static void getSchedule(Meeting[] meetings) {
+		Arrays.sort(meetings);
+
+		maxCnt++;
+		int prev = meetings[0].end;
+		
+		for (int i = 1; i < meetings.length; i++) {
+			if (prev <= meetings[i].start) {
+				maxCnt++;
+				prev = meetings[i].end;
+			}
+		}
+	}
+}
