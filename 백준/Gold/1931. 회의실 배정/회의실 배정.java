c1887faf1aa2e5daf7fe8ce53cbb4452e39d718a
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-02-17 00:37:20 +0900

    [Gold V] Title: 회의실 배정, Time: 436 ms, Memory: 44040 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -6,22 +6,14 @@ public class Main {
 	static Meeting[] meetings;
 	// 클래스 
 	static class Meeting implements Comparable<Meeting>{
-		// implements Comparable<T> : 내가 만든 Meeting 객체끼리 서로 어떻게 비교할지 내가 직접 정의해볼게!
-		// 기본 타입(int, double)은 값을 기준으로 비교하지만, 객체의 경우 어떻게 비교할지 정의해줘야 함
 		int start, end; // 시작 시간, 종료 시간
 
 		// 생성자
 		public Meeting(int start, int end) {
-			super();
 			this.start = start;
 			this.end = end;
 		}
 
-		@Override
-		public String toString() {
-			return "Meeting [start=" + start + ", end=" + end + "]";
-		}
-		
 		@Override
 		public int compareTo(Meeting o) {
 			// 종료시간이 빠른 순
@@ -46,13 +38,12 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			meetings[i] = new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 		}
-		
-		
-		getSchedule(meetings);
+        
+		getSchedule();
 		System.out.println(maxCnt);
 	}
 	
-	private static void getSchedule(Meeting[] meetings) {
+	private static void getSchedule() {
 		Arrays.sort(meetings);
 
 		maxCnt++;
