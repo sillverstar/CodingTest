5a719dad55d92a1aded6c70ff2618ef5c55afe0e
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-02 20:45:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 72 ms, Memory: 11644 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n, cnt;
+	static boolean[][] map, visited;
+	static List<Integer> homeCnt;
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		cnt = 0;
+		
+		map = new boolean[n][n];
+		visited = new boolean[n][n];
+		
+		homeCnt = new ArrayList<>();
+		
+		for (int i = 0; i < n; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < n; j++) {
+				map[i][j] = ((s.charAt(j) - '0') == 1);
+			}
+		}
+
+		
+
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if ((!visited[i][j]) && (map[i][j])) {
+					bfs(i, j);
+					cnt++;
+				}
+			}
+		}
+		
+
+		Collections.sort(homeCnt);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(cnt).append('\n');
+		for (int h : homeCnt) {
+			sb.append(h).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+	private static void bfs(int sr, int sc) {
+		int home = 0;
+		// 큐 생성
+		Deque<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {sr, sc});
+		visited[sr][sc] = true;
+		
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int r = cur[0];
+			int c = cur[1];
+			home++;
+			
+			for (int i = 0; i < 4; i++) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+				
+				// 범위 확인
+				if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
+				
+				// 집이 없는 경우 확인
+				if (!map[nr][nc]) continue;
+				
+				// 이미 방문한 경우 확인
+				if (visited[nr][nc]) continue;
+				
+				q.offer(new int[] {nr, nc});
+				visited[nr][nc] = true;
+			}
+		}
+		
+		// homeCnt 추가
+		homeCnt.add(home);
+		
+	}
+
+}
