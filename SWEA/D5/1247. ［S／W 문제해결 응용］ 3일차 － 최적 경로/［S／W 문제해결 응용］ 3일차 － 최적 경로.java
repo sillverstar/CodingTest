af8d279d2b89bdd79ef63dd81f5e70c89bb308b6
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-10 15:42:41 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 459 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	static int N, totalLen, dr, dc;
-	static int[][] cust;
+	static int N, minDist, dr, dc;
+	static int[][] customers;
 	static boolean[] visited;
 	
 	public static void main(String[] args) throws IOException {
@@ -21,18 +21,18 @@ public class Solution {
 			dr = Integer.parseInt(st.nextToken());
 			dc = Integer.parseInt(st.nextToken());
 			
-			cust = new int[N][2];
+			customers = new int[N][2];
 			for (int i = 0; i < N; i++) {
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
 				
-				cust[i] = new int[] {x, y};
+				customers[i] = new int[] {x, y};
 			}
-			totalLen = Integer.MAX_VALUE;
+			minDist = Integer.MAX_VALUE;
 			visited = new boolean[N];
-			dfs(sr, sc, 0, 0); // 좌표, cnt, len
+			dfs(sr, sc, 0, 0); // 현재 위치, 방문한 고객 수, 누적 거리
 			
-			sb.append('#').append(t).append(' ').append(totalLen).append('\n');
+			sb.append('#').append(t).append(' ').append(minDist).append('\n');
 		}
 		System.out.println(sb);
 	}
@@ -42,15 +42,18 @@ public class Solution {
 		// 종료 조건.
 		if (cnt == N) {
 			int nextLen = len + Math.abs(r - dr) + Math.abs(c - dc);
-			totalLen = Math.min(totalLen, nextLen);
+			minDist = Math.min(minDist, nextLen);
 			return;
 		}
 		
+		// 가지치기 추가: 현재 누적 거리가 지금까지 찾은 최솟값보다 크거나 같으면 다음을 탐색할 필요가 없음
+		if (len >= minDist) return;
+		
 		for (int i = 0; i < N; i++) {
 			if (visited[i]) continue;
-			int nextLen = len + Math.abs(r - cust[i][0]) + Math.abs(c - cust[i][1]);
+			int nextLen = len + Math.abs(r - customers[i][0]) + Math.abs(c - customers[i][1]);
 			visited[i] = true;
-			dfs(cust[i][0], cust[i][1], cnt + 1, nextLen);
+			dfs(customers[i][0], customers[i][1], cnt + 1, nextLen);
 			visited[i] = false;
 		}
 	}
