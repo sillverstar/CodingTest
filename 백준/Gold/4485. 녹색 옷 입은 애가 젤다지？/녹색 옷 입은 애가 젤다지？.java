8ca3de170cf72872239ec44c5629e204efd85c58
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-12 15:16:23 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 284 ms, Memory: 22992 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -50,6 +50,8 @@ public class Main {
 			int c = cur[1];
 			int cost = cur[2];
 			
+			if (cost > dist[r][c]) continue; // 추가
+			
 			for (int i = 0; i < 4; i++) {
 				int nr = r + dr[i];
 				int nc = c + dc[i];
