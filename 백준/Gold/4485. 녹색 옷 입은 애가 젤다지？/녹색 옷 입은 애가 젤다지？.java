4cd60d8a9d5083c7272af3236632f4ce501d7a2f
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-12 15:09:17 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 272 ms, Memory: 22728 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,87 +2,68 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int n, minCost;
-	static int[][] map, costs;
+	static int n;
+	static int[][] map, dist;
 	
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		int t = 1;
+		
 		StringBuilder sb = new StringBuilder();
+		int t = 1;
 		while (true) {
 			n = Integer.parseInt(br.readLine());
 			if (n == 0) break;
 			
-			minCost = Integer.MAX_VALUE;
 			map = new int[n][n];
-			costs = new int[n][n];
-			
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < n; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					costs[i][j] = Integer.MAX_VALUE;
 				}
 			}
+			dist = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(dist[i], Integer.MAX_VALUE);
+			}
 			
-			
-//			for (int i = 0; i < n; i++) {
-//				System.out.println(Arrays.toString(map[i]));
-//			}
-			
-			bfs();
-			
-			sb.append("Problem ").append(t).append(": ").append(costs[n-1][n-1]).append('\n');
+			dijkstra();
+			sb.append("Problem ").append(t).append(": ").append(dist[n-1][n-1]).append('\n');
 			t++;
-			
 		}
-		
 		System.out.println(sb);
-		
-		
-
 	}
 
-	private static void bfs() {
-		PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
-		// 가격이 저렴한 것부터 확인해야함. 그냥 큐로 하면 안됨
+
+	private static void dijkstra() {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+		// 초기값. {r, c, cost}
+		dist[0][0] = map[0][0];
+		pq.offer(new int[] {0, 0, map[0][0]});
 		
-		int sr = 0, sc = 0;
-		int sCost = map[sr][sc];
-		q.offer(new int[] {sr, sc, sCost});
-		
-		while (!q.isEmpty()) {
-			int[] cur = q.poll();
+		while (!pq.isEmpty()) {
+			int[] cur = pq.poll();
 			int r = cur[0];
 			int c = cur[1];
 			int cost = cur[2];
-			
-			if (cost > costs[r][c]) continue;
-			
-			// 끝까지 탐색
-			if (r == n-1 && c == n-1) {
-				return;
-			}
 			
 			for (int i = 0; i < 4; i++) {
 				int nr = r + dr[i];
 				int nc = c + dc[i];
 				
-				// 범위
 				if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
 				
-
-				int tempCost = cost + map[nr][nc];
-				if (tempCost < costs[nr][nc]) { // 저장된 금액보다 현재 경로의 비용이 더 싸면
-					costs[nr][nc] = tempCost;
-					q.offer(new int[] {nr, nc, tempCost} );
+				int nextCost = map[nr][nc] + cost;
+				if (dist[nr][nc] > nextCost) {
+					dist[nr][nc] = nextCost;
+					pq.offer(new int[] {nr, nc, nextCost});
 				}
-				
 			}
+			
 		}
 	}
+
 }
