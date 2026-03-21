74cc4b84bb0e1b5b4c1a6a1f208db4f00b2e74aa
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-21 21:55:16 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 572 ms, Memory: 64096 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,34 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken()); // 배열 개수
-		int m = Integer.parseInt(st.nextToken()); // 입력 개수
-
-		// 배열 입력 -> 바로 누적합 배열 만들기
-		int[] ps = new int[n + 1]; // arr[0]은 누적합 계산용
+		// 입력
+		int n = Integer.parseInt(st.nextToken()); // 수의 개수
+		int m = Integer.parseInt(st.nextToken()); // 구해야 하는 횟수
+		
+		// 배열 입력
+		int[] arr = new int[n+1];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= n; i++) {
-			ps[i] = ps[i - 1] + Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int[] result = new int[m];
+		// 누적합 계산
+		for (int i = 1; i <= n; i++) {
+			arr[i] = arr[i-1] + arr[i];
+		}
+		
+		
+		// 구간 계산
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
+			
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			result[i] = ps[end] - ps[start - 1];
+			
+			sb.append(arr[end] - arr[start-1]).append('\n');
 		}
-		
-		StringBuilder sb  = new StringBuilder();
-		for (int r : result) {
-			sb.append(r).append('\n');
-		}
-		System.out.print(sb);	
-		br.close();
+		System.out.println(sb);
 	}
 }
