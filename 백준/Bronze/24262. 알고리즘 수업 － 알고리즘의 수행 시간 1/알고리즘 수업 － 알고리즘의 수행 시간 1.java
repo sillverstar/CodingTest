bb7702b68137afaec1e68b565a40632fc7ed60ac
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-03-21 20:40:26 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+	static int n, count;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		
+		System.out.println(1);
+		System.out.println(0);
+	}
+}
