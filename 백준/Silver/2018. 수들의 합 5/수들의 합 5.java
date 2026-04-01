57601409c1f7d7a0b44b844d4c603324653e8e3c
sillverstar <108616794+sillverstar@users.noreply.github.com>
2026-04-01 23:23:00 +0900

    [Silver V] Title: 수들의 합 5, Time: 156 ms, Memory: 50932 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -2,33 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int startIdx = 1;
-		int endIdx = 1;
-		
-		int count = 1;
-		int sum = 1; // N 자기 자신 하나로 이루어진 경우의 수 미리 저장
-		
-		while (endIdx != N) {
-			if (sum == N) {
-				count++;
-				endIdx++;
-				sum += endIdx;
-			}
-			else if (sum > N) {
-				sum -= startIdx;
-				startIdx++;
-			}
-			else if (sum < N) {
-				endIdx++;
-				sum += endIdx;
-			}
-		}
-		System.out.println(count);
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N+1];
+        
+        for (int i = 0; i <= N; i++) {
+        	arr[i] = i;
+        }
+
+        int startIdx = 1;
+        int endIdx = 1;
+        
+        int count = 1; // N이 하나만 들어있을 때
+        int sum = 1; // 초기값
+        
+        while (startIdx != N) {
+        	if (sum == N) {
+        		count++;
+        		endIdx++;
+        		sum += arr[endIdx];
+        	}
+        	else if (sum > N) {
+        		sum -= arr[startIdx];
+        		startIdx++;
+        	}
+        	else if (sum < N) {
+        		endIdx++;
+        		sum += arr[endIdx];
+        	}
+        }
+        
+        System.out.println(count);
+        
+        br.close();
+        
+
+    }
 }
